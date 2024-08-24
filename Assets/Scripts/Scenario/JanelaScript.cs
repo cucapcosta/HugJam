cfd42be22e794b934157ec835d491848331f0ef5
cucapcosta <lucas.costa@sou.inteli.edu.br>
2024-08-24 11:07:52 -0300

    Algumas mudanças
    Quero que se foda conventional commits

--- Assets/Scripts/Scenario/JanelaScript.cs
+++ Assets/Scripts/Scenario/JanelaScript.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class JanelaScript : MonoBehaviour
 {
     public Animator animator;
-    void Start(){
+    public GameObject janelaQuebrando;
+    void Start()
+    {
         animator = GetComponent<Animator>();
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Tiro"))
         {
-            int randValue = Random.Range(0,2);
+            int randValue = Random.Range(0, 2);
             print("Random value: " + randValue);
             if (randValue == 0)
             {
                 animator.CrossFade("shattered1", 0);
-            }else{
+            }
+            else
+            {
                 animator.CrossFade("shattered2", 0);
             }
+            GameObject shatterInstance = Instantiate(janelaQuebrando, transform.position, Quaternion.identity);
             GetComponent<BoxCollider2D>().enabled = false;
             Destroy(collision.gameObject);
         }
